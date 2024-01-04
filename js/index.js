48731c4edc7af41d62d435ea6e2f96c67735f1d3
dahan <hongdh0514@naver.com>
2024-01-04 14:39:55 +0900

    feat: projects 마무리

--- js/index.js
+++ js/index.js
@@ -39,10 +39,22 @@ const descriptionContainers = [
     document.getElementById('description3')
 ];
 
+const explainTextContainers = [
+    document.getElementById('explain_text1'),
+    document.getElementById('explain_text2'),
+    document.getElementById('explain_text3')
+];
+
 const imagesSets = [
-    ['./img/icon_addr.png', './img/icon_java.png', './img/icon_callender.png'],
-    ['./img/FreeBoardList.jpeg', './img/icon_message.png', './img/icon_person.png'],
-    ['./img/icon_jsp.png', './img/icon_phone.png', './img/desk-593327_1280.jpg']
+    // 이웃 사촌
+    ['./img/FreeBoardList.jpeg', './img/FreeBoardView.jpeg', './img/FreeBoardWrite.jpeg', './img/FreeBoardModify.jpeg'],
+
+    // Fithub
+    ['./img/AdminMypage_ReportList.png', './img/AdminMypage_ReportProcess.png', './img/BoardFunction_Reportpng.png',
+     './img/AdditionaFunction_HealthCenter.png', './img/AdditionaFunction_BMI.png'],
+
+    //  포트폴리오
+    ['./img/portfolio_screenshot1.png', './img/portfolio_screenshot2.png', './img/portfolio_screenshot3.png', './img/portfolio_screenshot4.png']
 ];
 
 let currentIndexes = [-1, -1, -1];
@@ -74,9 +86,65 @@ initializeImage(3); // 세 번째 박스 초기화
 function projectDescription(boxIndex) {
     imgBoxContainers[boxIndex - 1].style.display = `none`;
     descriptionContainers[boxIndex - 1].style.display = `block`;
+    explainTextContainers[boxIndex - 1].innerHTML = `*설명 클릭 시 이미지*`;
 }
 
 function showImgBox(boxIndex) {
     imgBoxContainers[boxIndex - 1].style.display = `flex`;
     descriptionContainers[boxIndex - 1].style.display = `none`;
+    explainTextContainers[boxIndex - 1].innerHTML = `*이미지 클릭 시 확대&축소 / 더블클릭 시 설명*`;
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    // 스크롤이 200px 이상 되면 버튼을 표시합니다.
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+var zoomedIn = false;
+
+function toggleZoom(boxIndex) {
+    
+    if (!zoomedIn) {
+        zoomedIn = true;
+        document.getElementById("left_arrow_icon" + boxIndex).style.display = "none";
+        document.getElementById("right_arrow_icon" + boxIndex).style.display = "none";
+        document.getElementById("image-container" + boxIndex).style.width = "100%";
+        document.getElementById("image-container" + boxIndex).style.height = "100vh";
+    } 
+    else {
+        zoomedIn = false;
+        document.getElementById("left_arrow_icon" + boxIndex).style.display = "block";
+        document.getElementById("right_arrow_icon" + boxIndex).style.display = "block";
+        document.getElementById("image-container" + boxIndex).style.width = "50%";
+        document.getElementById("image-container" + boxIndex).style.height = "50vh";
+    }
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(boxIndex) {
+
+  imgBoxContainers[boxIndex - 1].style.display = `none`;
+  descriptionContainers[boxIndex - 1].style.display = `block`;
+}
+
+function test() {
+    alert("test");
 }
